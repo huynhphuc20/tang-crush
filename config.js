1c5cd9f3259242374d0502e640ffa80758be2020
huynhphuc20 <85377894+huynhphuc20@users.noreply.github.com>
2021-06-24 15:46:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'ê ê',
+    introTitle: 'hé lô',
     introDesc: `cái web này tạo ra để ngta tỏ tình crush
     nhưng mà mình hong phải ngta 
-    nên mình dùng nó để =)))`,
+    nên mình dùng nó để hỏi thăm bạn`,
     btnIntro: 'xem tiếp ha',
     title: 'chuyện là như này nè bạn êy 😙',
-    desc: 'mình có nói gòi là mình nhớ bạn vcl mà giờ dùng cách này nói lại cho nó thành thực ha',
-    btnYes: 'yes yes i am missing you vailon too',
-    btnNo: 'no no go away',
-    question:'hết dịch set kèo ngeeeee, hong bùm nựa sogi =))',
+    desc: 'dịch ở nhà làm gì cả ngày thếeeeee??? nhớ tôi không hả >:<',
+    btnYes: 'có chứ bạn iu iu',
+    btnNo: 'không, ra chỗ khác chơi',
+    question:'hết dịch gặp nhau ôm hun nhe =))',
     btnReply: 'trả lời thật lòng nào',
-    reply: 'cái này chịu hay không cũng phải chịu hà :)',
+    reply: 'ok bà luôn =))',
     mess: 'gòi ok pp',
-    messDesc: 'làm cái này kì công zl đó ',
-    btnAccept: 'iu thưn',
-    messLink: 'https://m.me/NPneee' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'làm cái này kì công lắm đó ',
+    btnAccept: 'ehehe cảm mơnn',
+    messLink: 'https://m.me/tamhauhehe' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
